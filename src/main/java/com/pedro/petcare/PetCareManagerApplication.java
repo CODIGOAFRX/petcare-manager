36f3901d1c6a7c8ed56c17dc8e8b85021969dd22
Pedro Gómez <pedrojgomezperez@gmail.com>
2025-12-05 12:09:57 +0100

    Initial commit

---
+++ src/main/java/com/pedro/petcare/PetCareManagerApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.petcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetCareManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetCareManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedro/petcare/PetCareManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.petcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetCareManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
